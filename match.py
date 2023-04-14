a2cadda7e38c1ed5dacb473aaec346660025754e
James Kelly <jkelly@sensoryinc.com>
2023-04-14 12:27:30 +0200

    reodering init steps and reformatting spaces to 4

--- match.py
+++ match.py
@@ -60,187 +60,192 @@ class ModelConfig(NamedTuple):
   
 
 class CTMatch:
-  def __init__(self, model_config: ModelConfig) -> None:
-    self.model_config = model_config
-    self.tokenizer = AutoTokenizer.from_pretrained(model_config.model_checkpoint)
-    self.ct_dataset = self.load_data()
-    self.ct_dataset_df = self.ct_dataset["train"].to_pandas()
-    self.model = self.load_model()
-    self.optimizer = AdamW(self.model.parameters(), lr=self.model_config.learning_rate, weight_decay=self.model_config.weight_decay)
-    self.num_training_steps = model_config.train_epochs * len(self.ct_dataset['train'])
-    self.lr_schedule = get_scheduler(
-       name="linear", optimizer=self.optimizer, num_warmup_steps=0, num_training_steps=self.num_training_steps
-    )
+    def __init__(self, model_config: ModelConfig) -> None:
+        self.model_config = model_config
+        self.tokenizer = AutoTokenizer.from_pretrained(model_config.model_checkpoint)
+        self.ct_dataset = self.load_data()
+        self.ct_dataset_df = self.ct_dataset["train"].to_pandas()
+        self.optimizer = None
+        self.lr_schedule = None
+        self.num_training_steps = self.model_config.train_epochs * len(self.ct_dataset['train'])
+        self.model = self.load_model()
+        
+        if self.trainer is False:
+            self.train_dataloader, self.val_dataloader = self.get_dataloaders()
 
-    if self.trainer is False:
-      self.train_dataloader, self.val_dataloader = self.get_dataloaders()
-
-    self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    self.model.to_device(self.device)
+        self.device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+        self.model.to_device(self.device)
 
 
-   # ------------------ native torch training loop ------------------ #
-  def get_dataloaders(self) -> Tuple[DataLoader, DataLoader]:
-    train_dataloader = DataLoader(self.ct_dataset['train'], shuffle=True, batch_size=self.model_config.batch_size)
-    val_dataloader = DataLoader(self.ct_dataset['val'], batch_size=self.model_config.batch_size)
-    return train_dataloader, val_dataloader
+     # ------------------ native torch training loop ------------------ #
+    def get_dataloaders(self) -> Tuple[DataLoader, DataLoader]:
+        train_dataloader = DataLoader(self.ct_dataset['train'], shuffle=True, batch_size=self.model_config.batch_size)
+        val_dataloader = DataLoader(self.ct_dataset['val'], batch_size=self.model_config.batch_size)
+        return train_dataloader, val_dataloader
 
-  def torch_train(self):
-    progress_bar = tqdm(range(self.num_training_steps))
-    self.model.train()
-    for _ in range(self.model_config.train_epochs):
-      for batch in self.train_dataloader:
-        batch = {k: v.to(self.device) for k, v in batch.items()}
-        outputs = self.model(**batch)
-        loss = outputs.loss
-        loss.backward()
+    def torch_train(self):
+        progress_bar = tqdm(range(self.num_training_steps))
+        self.model.train()
+        for _ in range(self.model_config.train_epochs):
+            for batch in self.train_dataloader:
+                batch = {k: v.to(self.device) for k, v in batch.items()}
+                outputs = self.model(**batch)
+                loss = outputs.loss
+                loss.backward()
 
-        self.optimizer.step()
-        self.lr_scheduler.step()
-        self.optimizer.zero_grad()
-        progress_bar.update(1)
+                self.optimizer.step()
+                self.lr_scheduler.step()
+                self.optimizer.zero_grad()
+                progress_bar.update(1)
 
-  def torch_eval(self):
-    metric = evaluate.load("f1")
-    self.model.eval()
-    for batch in self.val_dataloader:
-      batch = {k: v.to(self.device) for k, v in batch.items()}
-      
-      # don't learn during evaluation
-      with torch.no_grad():
-        outputs = self.model(**batch)
 
-      logits = outputs.logits
-      predictions = torch.argmax(logits, dim=-1)
-      metric.add_batch(predictions=predictions, references=batch["labels"])
+    def torch_eval(self):
+        metric = evaluate.load("f1")
+        self.model.eval()
+        for batch in self.val_dataloader:
+            batch = {k: v.to(self.device) for k, v in batch.items()}
+            
+            # don't learn during evaluation
+            with torch.no_grad():
+                outputs = self.model(**batch)
 
-    metric.compute()
+            logits = outputs.logits
+            predictions = torch.argmax(logits, dim=-1)
+            metric.add_batch(predictions=predictions, references=batch["labels"])
+
+        metric.compute()
 
 
 
   
 
-   
-  # ------------------ Data Loading ------------------ #
-  def load_data(self) -> Dataset:
-    self.ct_dataset = load_dataset('json', data_files=self.model_config.data_path.as_posix())
-    self.ct_dataset = train_test_val_split(self.ct_dataset, self.model_config.splits, self.model_config.seed)
-    self.add_features()
-    self.tokenize_dataset()
-    self.ct_dataset.rename_column("label", "labels")
-    self.ct_dataset.rename_column("topic", "sentence1")
-    self.ct_dataset.rename_column("doc", "sentence2")
-    return self.ct_dataset
+    
+    # ------------------ Data Loading ------------------ #
+    def load_data(self) -> Dataset:
+        self.ct_dataset = load_dataset('json', data_files=self.model_config.data_path.as_posix())
+        self.ct_dataset = train_test_val_split(self.ct_dataset, self.model_config.splits, self.model_config.seed)
+        self.add_features()
+        self.tokenize_dataset()
+        self.ct_dataset.rename_column("label", "labels")
+        self.ct_dataset.rename_column("topic", "sentence1")
+        self.ct_dataset.rename_column("doc", "sentence2")
+        return self.ct_dataset
  
 
 
     
 
 
-  def add_features(self) -> None:
-    features = Features({
-        'doc': Value(dtype='string', id=None),
-        'label': ClassLabel(names=["not_relevant", "partially_relevant", "relevant"]),
-        'topic': Value(dtype='string', id=None)
-      })
-    self.ct_dataset["train"] = self.ct_dataset["train"].map(lambda x: x, batched=True, features=features)
-    self.ct_dataset["test"] = self.ct_dataset["test"].map(lambda x: x, batched=True, features=features)
-    self.ct_dataset["validation"] = self.ct_dataset["validation"].map(lambda x: x, batched=True, features=features)  
+    def add_features(self) -> None:
+        features = Features({
+            'doc': Value(dtype='string', id=None),
+            'label': ClassLabel(names=["not_relevant", "partially_relevant", "relevant"]),
+            'topic': Value(dtype='string', id=None)
+        })
+        self.ct_dataset["train"] = self.ct_dataset["train"].map(lambda x: x, batched=True, features=features)
+        self.ct_dataset["test"] = self.ct_dataset["test"].map(lambda x: x, batched=True, features=features)
+        self.ct_dataset["validation"] = self.ct_dataset["validation"].map(lambda x: x, batched=True, features=features)  
 
 
-  def tokenize_function(self, examples):
-    return self.tokenizer(
-      examples["doc"], examples["topic"], 
-      truncation=self.model_config.truncation, 
-      padding=self.model_config.padding, 
-      max_length=self.model_config.max_length
-    )
+    def tokenize_function(self, examples):
+        return self.tokenizer(
+            examples["doc"], examples["topic"], 
+            truncation=self.model_config.truncation, 
+            padding=self.model_config.padding, 
+            max_length=self.model_config.max_length
+        )
 
-  def tokenize_dataset(self):
-    self.ct_dataset = self.ct_dataset.map(self.tokenize_function, batched=True)
-
-
-  # ------------------ Model Loading ------------------ #
-  def load_model(self):
-    id2label, label2id = self.get_label_mapping()
-    self.model = AutoModelForSequenceClassification.from_pretrained(
-      self.model_config.model_checkpoint,
-      num_labels=3,                                    # makes the last head be replaced with a linear layer with 3 outputs
-      id2label=id2label, label2id=label2id
-    )
-    self.trainer = self.get_trainer()
-    return self.trainer
+    def tokenize_dataset(self):
+        self.ct_dataset = self.ct_dataset.map(self.tokenize_function, batched=True)
 
 
-  def get_label_mapping(self):
-    id2label = {idx:self.ct_dataset['train'].features["label"].int2str(idx) for idx in range(3)}
-    label2id = {v:k for k, v in id2label.items()}
-    return id2label, label2id
+    # ------------------ Model Loading ------------------ #
+    def load_model(self):
+        id2label, label2id = self.get_label_mapping()
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            self.model_config.model_checkpoint,
+            num_labels=3,                                    # makes the last head be replaced with a linear layer with 3 outputs
+            id2label=id2label, label2id=label2id
+        )
+        self.optimizer = AdamW(self.model.parameters(), lr=self.model_config.learning_rate, weight_decay=self.model_config.weight_decay)
+        self.num_training_steps = self.model_config.train_epochs * len(self.ct_dataset['train'])
+        self.lr_schedule = get_scheduler(
+            name="linear", optimizer=self.optimizer, num_warmup_steps=0, num_training_steps=self.num_training_steps
+        )
 
-  def get_label_weights(self):
-    label_weights = (1 - (self.ct_dataset_df["label"].value_counts().sort_index() / len(self.ct_dataset_df))).values
-    label_weights = torch.from_numpy(label_weights).float().to("cuda")
+        self.trainer = self.get_trainer()
+        return self.trainer
 
 
-  def get_trainer(self):
-    return WeightedLossTrainer(
-      model=self.model,
-      optimizers=(self.optimizer, self.lr_scheduler),
-      args=self.get_training_args_obj(),
-      compute_metrics=compute_metrics,
-      train_dataset=self.ct_dataset["train"],
-      eval_dataset=self.ct_dataset["validation"],
-      tokenizer=self.tokenizer,
-      label_weights=self.get_label_weights()
-    )
+    def get_label_mapping(self):
+        id2label = {idx:self.ct_dataset['train'].features["label"].int2str(idx) for idx in range(3)}
+        label2id = {v:k for k, v in id2label.items()}
+        return id2label, label2id
 
-  def get_training_args_obj(self):
-    output_dir = self.model_config.output_dir if self.model_config.output_dir is not None else self.model_config.data_path.parent.parent.as_posix()
+    def get_label_weights(self):
+        label_weights = (1 - (self.ct_dataset_df["label"].value_counts().sort_index() / len(self.ct_dataset_df))).values
+        label_weights = torch.from_numpy(label_weights).float().to("cuda")
+
+
+    def get_trainer(self):
+        return WeightedLossTrainer(
+            model=self.model,
+            optimizers=(self.optimizer, self.lr_scheduler),
+            args=self.get_training_args_obj(),
+            compute_metrics=compute_metrics,
+            train_dataset=self.ct_dataset["train"],
+            eval_dataset=self.ct_dataset["validation"],
+            tokenizer=self.tokenizer,
+            label_weights=self.get_label_weights()
+        )
+
+    def get_training_args_obj(self):
+        output_dir = self.model_config.output_dir if self.model_config.output_dir is not None else self.model_config.data_path.parent.parent.as_posix()
+        
+        return TrainingArguments(
+            output_dir=output_dir,
+            num_train_epochs=self.model_config.num_train_epochs,
+            learning_rate=self.model_config.learning_rate,
+            per_device_train_batch_size=self.model_config.batch_size,
+            per_device_eval_batch_size=self.model_config.batch_size,
+            weight_decay=self.model_config.weight_decay,
+            evaluation_strategy="epoch",
+            logging_steps=len(self.ct_dataset["train"]) // self.model_config.batch_size,
+        )
+        
+
+    def build_confusion_matrix(self):
+        labels = self.ct_dataset['train'].features["label"].names
+        
+        y_preds = []  
+        y_trues = []
+        for index, val_text in enumerate(self.ct_dataset['val']['texts']):
+            tokenized_val_text = self.tokenizer(
+                                    [val_text], 
+                                    truncation=self.model_config.truncation,
+                                    padding=self.model_config.padding,
+                                    return_tensor='pt'
+                                )
+            logits = self.model(tokenized_val_text)
+            prediction = F.softmax(logits, dim=1)
+            y_pred = torch.argmax(prediction).numpy()
+            y_true = self.ct_dataset['val']['label'][index]
+            y_preds.append(y_pred)
+            y_trues.append(y_true)
     
-    return TrainingArguments(
-      output_dir=output_dir,
-      num_train_epochs=self.model_config.num_train_epochs,
-      learning_rate=self.model_config.learning_rate,
-      per_device_train_batch_size=self.model_config.batch_size,
-      per_device_eval_batch_size=self.model_config.batch_size,
-      weight_decay=self.model_config.weight_decay,
-      evaluation_strategy="epoch",
-      logging_steps=len(self.ct_dataset["train"]) // self.model_config.batch_size,
-    )
-    
+        return confusion_matrix(y_trues, y_preds, labels=labels)
 
-  def build_confusion_matrix(self):
-    labels = self.ct_dataset['train'].features["label"].names
-    
-    y_preds = []  
-    y_trues = []
-    for index, val_text in enumerate(self.ct_dataset['val']['texts']):
-        tokenized_val_text = self.tokenizer(
-                                [val_text], 
-                                truncation=self.model_config.truncation,
-                                padding=self.model_config.padding,
-                                return_tensor='pt'
-                            )
-        logits = self.model(tokenized_val_text)
-        prediction = F.softmax(logits, dim=1)
-        y_pred = torch.argmax(prediction).numpy()
-        y_true = self.ct_dataset['val']['label'][index]
-        y_preds.append(y_pred)
-        y_trues.append(y_true)
-    
-    return confusion_matrix(y_trues, y_preds, labels=labels)
-
-  # ------------------ Embedding Similarity ------------------ #
-  def get_embedding_similarity(self, topic, document):
-      topic_input = self.tokenizer(topic, return_tensors='pt').to('cuda')
-      doc_input = self.tokenizer(document, return_tensors='pt').to('cuda')
-      topic_output = self.model(**topic_input, output_hidden_states=True)
-      doc_output = self.model(**doc_input, output_hidden_states=True)
-      topic_last_hidden = np.squeeze(topic_output.hidden_states[-1].detach().cpu().numpy(), axis=0)
-      doc_last_hidden = np.squeeze(doc_output.hidden_states[-1].detach().cpu().numpy(), axis=0)
-      topic_emb = np.mean(topic_last_hidden, axis=0)
-      doc_emb = np.mean(doc_last_hidden, axis=0)
-      return dot(topic_emb, doc_emb)/(norm(topic_emb) * norm(doc_emb))
+    # ------------------ Embedding Similarity ------------------ #
+    def get_embedding_similarity(self, topic, document):
+        topic_input = self.tokenizer(topic, return_tensors='pt').to('cuda')
+        doc_input = self.tokenizer(document, return_tensors='pt').to('cuda')
+        topic_output = self.model(**topic_input, output_hidden_states=True)
+        doc_output = self.model(**doc_input, output_hidden_states=True)
+        topic_last_hidden = np.squeeze(topic_output.hidden_states[-1].detach().cpu().numpy(), axis=0)
+        doc_last_hidden = np.squeeze(doc_output.hidden_states[-1].detach().cpu().numpy(), axis=0)
+        topic_emb = np.mean(topic_last_hidden, axis=0)
+        doc_emb = np.mean(doc_last_hidden, axis=0)
+        return dot(topic_emb, doc_emb)/(norm(topic_emb) * norm(doc_emb))
 
 
 
@@ -254,11 +259,11 @@ class CTMatch:
 
 
 def train_ctmatch_classifier(model_config: ModelConfig):
-  ctmatch_dataset, ctmatch_model = CTMatch(model_config)
-  ctmatch_model.train()
-  predictions = ctmatch_model.predict(ctmatch_dataset["test"])
-  print(predictions.metrics.items())
-  return ctmatch_model, ctmatch_dataset
+    ctmatch_dataset, ctmatch_model = CTMatch(model_config)
+    ctmatch_model.train()
+    predictions = ctmatch_model.predict(ctmatch_dataset["test"])
+    print(predictions.metrics.items())
+    return ctmatch_model, ctmatch_dataset
 
 
 
@@ -275,15 +280,15 @@ if __name__ == '__main__':
     scibert_model = 'allenai/scibert_scivocab_uncased'
 
     config = ModelConfig(
-      data_path=KZ_DATA,
-      model_checkpoint=scibert_model,
-      max_length=512,
-      batch_size=16,
-      learning_rate=2e-5,
-      train_epochs=3,
-      weight_decay=0.01,
-      warmup_steps=500,
-      splits={"train":0.8, "val":0.1}
+        data_path=KZ_DATA,
+        model_checkpoint=scibert_model,
+        max_length=512,
+        batch_size=16,
+        learning_rate=2e-5,
+        train_epochs=3,
+        weight_decay=0.01,
+        warmup_steps=500,
+        splits={"train":0.8, "val":0.1}
     )
 
     train_ctmatch_classifier(config)

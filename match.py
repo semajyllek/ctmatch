2db432eaead1e1d7737aeebdfd47f0036a1494a9
James Kelly <jkelly@sensoryinc.com>
2023-04-15 11:43:34 +0200

    torch formatting

--- match.py
+++ match.py
@@ -162,7 +162,7 @@ class CTMatch:
         self.ct_dataset = self.ct_dataset.rename_column("label", "labels")
         # self.ct_dataset = self.ct_dataset.rename_column("topic", "sentence1")
         # self.ct_dataset = self.ct_dataset.rename_column("doc", "sentence2")
-        self.ct_dataset.set_format(type='torch', columns=['doc', 'label', 'topic', 'input_ids', 'attention_mask'])
+        self.ct_dataset.set_format(type='torch', columns=['doc', 'labels', 'topic', 'input_ids', 'attention_mask'])
         if not self.model_config.use_trainer:
             self.ct_dataset = self.ct_dataset.remove_columns(['doc', 'topic'])
 
@@ -235,7 +235,7 @@ class CTMatch:
         return id2label, label2id
 
     def get_label_weights(self):
-        label_weights = (1 - (self.ct_dataset_df["label"].value_counts().sort_index() / len(self.ct_dataset_df))).values
+        label_weights = (1 - (self.ct_dataset_df["labels"].value_counts().sort_index() / len(self.ct_dataset_df))).values
         label_weights = torch.from_numpy(label_weights).float().to("cuda")
 
 

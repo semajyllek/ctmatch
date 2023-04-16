b2e2b48575e12dc72027b8b5b91410ebb7f4f83c
James Kelly <jkelly@sensoryinc.com>
2023-04-16 17:36:00 +0200

    added gen category support to ctmatch, post-pre-processing

--- match.py
+++ match.py
@@ -183,7 +183,7 @@ class CTMatch:
         id2label, label2id = self.get_label_mapping()
         return AutoModelForSequenceClassification.from_pretrained(
             self.model_config.model_checkpoint,
-            um_labels=self.model_config.num_classes,     # makes the last head be replaced with a linear layer with num_labels outputs (fine-tuning)
+            num_labels=self.model_config.num_classes,     # makes the last head be replaced with a linear layer with num_labels outputs (fine-tuning)
             id2label=id2label, label2id=label2id
         )
         

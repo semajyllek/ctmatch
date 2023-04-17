ddd51bd7cc1084d196b4d143cdf3c38eaee77290
James Kelly <jkelly@sensoryinc.com>
2023-04-17 10:16:06 +0200

    package restructuring

--- ctmatch/match.py
+++ ctmatch/match.py
@@ -18,8 +18,8 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn import svm
 
-from ctmatch.ctmatch_utils import compute_metrics, train_test_val_split, l2_normalize
-from ctmatch.modelconfig import ModelConfig
+from utils.ctmatch_utils import compute_metrics, train_test_val_split, l2_normalize
+from modelconfig import ModelConfig
 
 
 

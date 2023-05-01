b353418da0d2e2f1d57d146744e45139ec8d5067
James Kelly <jkelly@sensoryinc.com>
2023-05-01 08:41:45 +0200

    added gpt-2 to list of usable sequence classifiers in dataprep.py

--- ctmatch/dataprep.py
+++ ctmatch/dataprep.py
@@ -14,7 +14,7 @@ from .modelconfig import ModelConfig
 SUPPORTED_LMS = [
     'roberta-large', 'cross-encoder/nli-roberta-base',
     'microsoft/biogpt', 'allenai/scibert_scivocab_uncased', 
-    'facebook/bart-large', 'gpt-2'
+    'facebook/bart-large', 'gpt2'
 ]
 
 

cfd41973b6afc630c4fde6dda89ca9b78a0be49d
James Kelly <jkelly@sensoryinc.com>
2023-05-02 17:15:04 +0200

    updated prep script

--- ctmatch/ctmatch_prep.py
+++ ctmatch/ctmatch_prep.py
@@ -1,14 +1,16 @@
 
 from typing import Any, Dict, List, NamedTuple, Optional, Set, Tuple, Union
-import ct_data_paths
+import ctproc_ct_data_paths as ctpaths
+import numpy as np
 import random
 import json
 
-#from ctproc.proc import CTConfig, CTProc, CTDocument, CTTopic
-from utils.ctmatch_utils import get_processed_data, truncate
-from scripts.vis_script import analyze_test_rels
-import eda
+from proc import CTConfig, CTProc, CTDocument, CTTopic
+from scripts.vis_scripts import analyze_test_rels
+from ctproc_ctmatch_utils import get_processed_data, truncate
+import ctproc_eda as eda
 
+LLM_END_PROMPT: str = "Revelance score (0, 1, or 2) : [CLS] "
 
 class DataConfig(NamedTuple):
 	save_path: str
@@ -33,7 +35,7 @@ class DataConfig(NamedTuple):
 
 def proc_docs_and_topics(trec_or_kz: str = 'trec') -> Tuple[Dict[str, Dict[str, str]], Dict[str, Dict[str, str]]]:
 
-	doc_tuples, topic_tuples = ct_data_paths.get_data_tuples(trec_or_kz)
+	doc_tuples, topic_tuples = ctpaths.get_data_tuples(trec_or_kz)
 
 	id2topic = dict()
 	for topic_source, topic_target in topic_tuples:
@@ -41,9 +43,9 @@ def proc_docs_and_topics(trec_or_kz: str = 'trec') -> Tuple[Dict[str, Dict[str, 
 		print(f"processed {trec_or_kz} topic source: {topic_source}, and wrote to {topic_target}")
 	
 	id2doc = dict()
-	for doc_source, doc_target in doc_tuples:
-		id2doc.update(proc_docs(doc_source, doc_target))
-		print(f"processed {trec_or_kz} doc source: {doc_source}, and wrote to {doc_target}")
+	# for doc_source, doc_target in doc_tuples:
+	# 	id2doc.update(proc_docs(doc_source, doc_target))
+	# 	print(f"processed {trec_or_kz} doc source: {doc_source}, and wrote to {doc_target}")
 
 
 	return id2topic, id2doc
@@ -82,11 +84,38 @@ def proc_topics(topic_path: str, output_path: str, trec_or_kz: str = 'trec') -> 
 
 
 
+def filter_doc_for_ir(doc, dconfig) -> Dict[str, List[str]]:
+	new_doc = dict()
+	new_doc['id'] = doc['id']
+	new_doc['text'] = prep_doc_text(doc, dconfig)
+	return new_doc
+
+
+def prep_ir_dataset(dconfig: DataConfig):
+	# need a file of all docs with their 
+	# 1. ids, 
+	# 2. combined text... 
+	# 3. 
+	
+	# get path to processed docs
+	doc_tuples, _ = ctpaths.get_data_tuples(dconfig.trec_or_kz)
+		
+	# get all processed docs
+	id2doc = dict()
+	for _, processed_doc_path in doc_tuples:
+		print(f"getting docs from {processed_doc_path}")
+		for doc in get_processed_data(processed_doc_path):
+			doc = filter_doc_for_ir(doc, dconfig)
+			doc['category'] = np.asarray(sorted(doc['category']).values())  # makes a consistently ordered category vector
+			id2doc[doc.id] = doc
+	return id2doc
+
+
 # --------------------------------------------------------------------------------------------------------------- #
 # pre-processing functions to save a form of triples for a particular model spec
 # --------------------------------------------------------------------------------------------------------------- #
 
-def prep_dataset(
+def prep_fine_tuning_dataset(
 		dconfig: DataConfig
 ) -> None:
 	"""
@@ -102,7 +131,7 @@ def prep_dataset(
 	rel_type_dict, rel_dict, all_qrelled_docs = analyze_test_rels(rel_path)
 	
 	# get path to processed docs (already got topic path)
-	doc_tuples, _ = ct_data_paths.get_data_tuples(dconfig.trec_or_kz)
+	doc_tuples, _ = ctpaths.get_data_tuples(dconfig.trec_or_kz)
 
 	# get mappings of doc ids to doc dicts and topic ids to topic dicts
 	id2doc, id2topic = get_doc_and_topic_mappings(all_qrelled_docs, doc_tuples, topic_path) 
@@ -194,15 +223,17 @@ def get_n_crit(crit_list: List[str], dconfig: DataConfig) -> List[str]:
 
 
 def prep_doc_text(doc: Dict[str, Union[List[str], str, float]], dconfig: DataConfig) -> str:
+	
 
 	# combine lists of strings into single string
-
 	doc_inc = ' '.join(get_n_crit(doc['elig_crit']['include_criteria'], dconfig))
 	doc_exc = ' '.join(get_n_crit(doc['elig_crit']['exclude_criteria'], dconfig))
 
 
 	if 'condition' in dconfig.filtered_doc_keys:
 		doc_inc = f"{' '.join(doc['condition'])} {doc_inc}"
+		if dconfig.llm_prep:
+			doc_inc = "Condition: " + doc_inc + ", "
 
 	#truncate criteria separately if in config
 	doc_inc = truncate(doc_inc, dconfig.max_inc_len)
@@ -211,19 +242,24 @@ def prep_doc_text(doc: Dict[str, Union[List[str], str, float]], dconfig: DataCon
 
 	if dconfig.prepend_elig_gender:
 		doc_inc = f"{doc['elig_gender']} {dconfig.sep} {doc_inc}"
+		if dconfig.llm_prep:
+			doc_inc = "Gender: " + doc_inc + ", "
 
 	if dconfig.prepend_elig_age:
 		if dconfig.llm_prep:
-			doc_inc = f"A person who is between {doc['elig_min_age']}-{doc['elig_max_age']} years old who meets the following Inclusion Criteria: {doc_inc}"
+			doc_inc = f"Trial Doc: A person who is between {doc['elig_min_age']}-{doc['elig_max_age']} years old who meets the following Inclusion Criteria: {doc_inc}"
 		else:
 			doc_inc = f"eligible ages (years): {doc['elig_min_age']}-{doc['elig_max_age']}, {dconfig.sep} {doc_inc}"
 
 	# combine criteria into single string
 	if dconfig.include_only:
+		if dconfig.llm_prep:
+			doc_inc += LLM_END_PROMPT
 		return doc_inc
 	
 	if dconfig.llm_prep:
-		return f"{doc_inc} and does not meet these Exclusion Criteria: {doc_exc}"
+		return f"{doc_inc} and does not meet these Exclusion Criteria: {doc_exc} {LLM_END_PROMPT}"
+
 	return f"{doc_inc} {dconfig.sep} {doc_exc}"
 
 	
@@ -250,11 +286,11 @@ def gender_match(doc_gender: str, topic_gender: str) -> bool:
 
 def get_topic_and_rel_path(trec_or_kz: str = 'trec') -> Tuple[str, str]:
 	if trec_or_kz == 'trec':
-		rel_path = ct_data_paths.TREC_REL_PATH
-		topic_path = ct_data_paths.TREC_RELLED_TOPIC_PATH
+		rel_path = ctpaths.TREC_REL_PATH
+		topic_path = ctpaths.TREC_RELLED_TOPIC_PATH
 	else:
-		rel_path = ct_data_paths.KZ_REL_PATH
-		topic_path = ct_data_paths.KZ_RELLED_TOPIC_PATH
+		rel_path = ctpaths.KZ_REL_PATH
+		topic_path = ctpaths.KZ_RELLED_TOPIC_PATH
 	return topic_path, rel_path
 
 
@@ -278,18 +314,39 @@ def get_doc_and_topic_mappings(all_qrelled_docs: Set[str], doc_tuples: List[Tupl
 
 
 if __name__ == '__main__':
-	# proc_docs_and_topics('trec')
-	eda.explore_trec_data(part=2, rand_print=0.001) # select part 1-5 (~70k docs per part)
+	# proc_docs_and_topics('kz')
+	# eda.explore_trec_data(part=2, rand_print=0.001) # select part 1-5 (~70k docs per part)
 	# eda.explore_kz_data(rand_print=0.00001) # all in one file (~200k docs)
 
-	# dconfig = DataConfig(
-	# 	trec_or_kz='kz',
-	# 	save_path=ct_data_paths.KZ_ML_PATH,
-	# 	sep='',
-	# 	first_n_only=2,
-	# )
-	# prep_dataset(dconfig)
-	#explore_prepped(ct_data_paths.TREC_ML_PATH)
 
+# class DataConfig(NamedTuple):
+# 	save_path: str
+# 	trec_or_kz: str = 'trec'
+# 	filtered_topic_keys: Set[str] = {'id', 'text_sents', 'age', 'gender'}
+# 	filtered_doc_keys: Set[str] = {'id', 'elig_min_age', 'elig_max_age', 'elig_gender', 'condition', 'elig_crit'}
+# 	max_topic_len: Optional[int] = None
+# 	max_inc_len: Optional[int] = None
+# 	max_exc_len: Optional[int] = None
+# 	prepend_elig_age: bool = True
+# 	prepend_elig_gender: bool = True
+# 	include_only: bool = False
+# 	downsample_zeros_n: Optional[int] = None
+# 	sep: str = '[SEP]'
+# 	llm_prep: bool = False
+# 	first_n_only: Optional[int] = None
+# 	convert_snli: bool = False
+# 	infer_category_model: Optional[str] = None
 
+	dconfig = DataConfig(
+		trec_or_kz='trec',
+		save_path=ctpaths.TREC_ML_PATH, # make sure to change this!
+		sep='',
+		first_n_only=10,
+		max_topic_len=200,
+		llm_prep=False,
+		prepend_elig_age=True,
+		prepend_elig_gender=False
+	)
+	prep_fine_tuning_dataset(dconfig)
+	#eda.explore_prepped(ctpaths.TREC_KZ_PATH)
 

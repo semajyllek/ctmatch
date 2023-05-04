3005e7eee8af51f2e9e1401af142fd2a1d979cee
James Kelly <jkelly@sensoryinc.com>
2023-05-04 22:45:15 +0200

    testing ir data setup

--- ctmatch/modelconfig.py
+++ ctmatch/modelconfig.py
@@ -21,9 +21,9 @@ class ModelConfig(NamedTuple):
     use_trainer: bool = True
     gen_model: str = 'gpt2'
     num_classes: int = 3
+    fp16: bool = False
     push_to_hub: bool = False
     ir_save_path: Optional[str] = None
-    
     processed_data_paths: Optional[List[str]] = None
 
 

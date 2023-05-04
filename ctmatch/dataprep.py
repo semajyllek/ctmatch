fc2e6edbab80ca4980261faed1a41e34a6971d82
James Kelly <jkelly@sensoryinc.com>
2023-05-04 23:20:48 +0200

    testing ir data setup

--- ctmatch/dataprep.py
+++ ctmatch/dataprep.py
@@ -14,7 +14,7 @@ from .modelconfig import ModelConfig
 SUPPORTED_LMS = [
     'roberta-large', 'cross-encoder/nli-roberta-base',
     'microsoft/biogpt', 'allenai/scibert_scivocab_uncased', 
-    'facebook/bart-large', 'gpt2', 'ctmatch_finetuned_allenai/scibert_scivocab_uncased-classifier'
+    'facebook/bart-large', 'gpt2', '/content/drive/MyDrive/finetuned_scibert_scivocab_uncasedmodel'
 ]
 
 

a65a4483b4ca3ff3346b08335e469ff8702e6a8a
James Kelly <jkelly@sensoryinc.com>
2023-05-05 09:41:53 +0200

    testing ir data setup

--- ctmatch/models/gen_model.py
+++ ctmatch/models/gen_model.py
@@ -63,6 +63,7 @@ class GenModel:
 
     def get_embedding(self, s: str):
         input = self.tokenizer(s, return_tensors='pt')
+        print(s)
         with torch.no_grad():
             embedding = self.model(input['input_ids']).hidden_states[-1]
 

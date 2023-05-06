6943593f4ca004343ddbe801bd2c3f9614e70848
James Kelly <jkelly@sensoryinc.com>
2023-05-06 23:12:59 +0200

    tookk out embeddings stuff from prep_ir to do elsewhere by batch

--- ctmatch/match.py
+++ ctmatch/match.py
@@ -159,8 +159,8 @@ class CTMatch:
                     print(f"Prepping doc {i}")
 
                 #ir_data_entry['doc_embedding'] = self.gen_model.get_embedding(doc_text)
-                print(doc['doc_text'])
-                wf.write(doc['doc_text'])
+                #print(doc['doc_text'])
+                wf.write(doc['doc_text'], 'utf-8')
                 wf.write('\n')
        
 

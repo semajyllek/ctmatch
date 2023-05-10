c5517bee26a67aef0a3d7b372456f659fc4935bd
James Kelly <jkelly@sensoryinc.com>
2023-05-11 00:08:41 +0200

    going back to testing because current version uses too much RAM and crashes colab :(

--- ctmatch/match.py
+++ ctmatch/match.py
@@ -165,6 +165,7 @@ class CTMatch:
     def get_embeddings(self, texts: List[str]) -> List[float]:
         return self.embedding_model.encode(texts)
     
+    @classmethod
     def get_categories(self, text: str) -> str:
         if self.category_model is None:
             self.category_model = pipeline(

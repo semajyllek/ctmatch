2315ec4731068418dc8ae7faf2c5753e580c6728
James Kelly <jkelly@sensoryinc.com>
2023-05-14 20:52:40 +0200

    updated app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 import gradio as gr
 
-from typing import Optional
 from ctmatch.match import CTMatch, PipeConfig
 
 
 
-def ctmatch_web_api(topic_query: str, top_k: int = 10, openai_api_key: Optional[str] = None) -> None:
+def ctmatch_web_api(topic_query: str) -> str:
     pipe_config = PipeConfig(
-        openai_api_key = openai_api_key,
         ir_setup=True,
+
         filters=['svm', "classifier"]
     )
     
     ctm = CTMatch(pipe_config)
-    return '\n'.join([f"{nid}: {txt}" for nid, txt in ctm.match_pipeline(topic_query, top_k=top_k)])
+    return '\n'.join([f"{nid}: {txt}" for nid, txt in ctm.match_pipeline(topic_query, top_k=5)])
 
+
+with gr.Blocks() as demo:
+    name = gr.Textbox(lines=5, label="patient description", placeholder="Patient is a 45-year-old man with a history of anaplastic astrocytoma...")
+    output = gr.Textbox(lines=10, label="matching trials")
+    greet_btn = gr.Button("match")
+    greet_btn.click(fn=ctmatch_web_api, inputs=name, outputs=output, api_name="match")
 
 demo = gr.Interface(fn=ctmatch_web_api, inputs="text", outputs="text")
 demo.launch()   

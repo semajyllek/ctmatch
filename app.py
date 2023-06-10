44bf68d4e2d4c34edd34a3ed75ee5076057983b1
James Kelly <jkelly@sensoryinc.com>
2023-06-10 12:11:44 +0200

    updated app.py to 1. not load object every time, 2. use pruned model

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-import gradio as gr
 
 from ctmatch.match import CTMatch, PipeConfig
+import gradio as gr
 
 
-
-def ctmatch_web_api(topic_query: str) -> str:
-   
+if __name__ == "__main__":
     pipe_config = PipeConfig(
+        classifier_model_checkpoint='semaj83/scibert_finetuned_pruned_ctmatch',
         ir_setup=True,
-        filters=['svm', "classifier"]
+        filters=["svm", "classifier"],
     )
-    
-    ctm = CTMatch(pipe_config)
-    return '\n'.join([f"{nid}: {txt}" for nid, txt in ctm.match_pipeline(topic_query, top_k=5)])
 
+    CTM = CTMatch(pipe_config)
 
-with gr.Blocks(css=".gradio-container {background-color: #00CED1}") as demo:
-    name = gr.Textbox(lines=5, label="patient description", placeholder="Patient is a 45-year-old man with a history of anaplastic astrocytoma...")
-    output = gr.Textbox(lines=10, label="matching trials")
-    greet_btn = gr.Button("match")
-    greet_btn.click(fn=ctmatch_web_api, inputs=name, outputs=output, api_name="match")
+    def ctmatch_web_api(topic_query: str) -> str:
+        return '\n\n'.join([f"{nid}: {txt}" for nid, txt in CTM.match_pipeline(topic_query, top_k=5)])
 
-demo.queue().launch(share=True)
+    with gr.Blocks(css=".gradio-container {background-color: #00CED1}") as demo:
+        name = gr.Textbox(lines=5, label="patient description", placeholder="Patient is a 45-year-old man with a history of anaplastic astrocytoma...")
+        output = gr.Textbox(lines=10, label="matching trials")
+        greet_btn = gr.Button("match")
+        greet_btn.click(fn=ctmatch_web_api, inputs=name, outputs=output, api_name="match")
+
+    demo.queue().launch(share=True, debug=True)
